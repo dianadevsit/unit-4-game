274d71db17271b00d82e803f50e2f6f5e5dbc7ac
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-02-09 17:21:29 -0500

    fixed some errors in the jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //adding variables for the crystals with wins and the total score
-$(document).ready(function() {
+$(document).ready(function () {
 	let magicNum;
 	let totalScore;
 	let wins = 0;
@@ -9,9 +9,6 @@ $(document).ready(function() {
 	let crystal3;
 	let crystal4;
 	newGame();
-	//creating functions -- using Math.floor(Math.random()
-	//using Math.ceil() function in JavaScript is used to round the number passed as parameter to its nearest integer in Upward direction of rounding i.e towards the greater value.
-	//to create the number randomly for the crystals and make sure they round up
 	function newNumbers() {
 		magicNum = Math.floor(Math.random() * 110 + 20);
 		crystal1 = Math.ceil(Math.random() * 12);
@@ -36,13 +33,13 @@ $(document).ready(function() {
 		console.log(crystal1, crystal2, crystal3, crystal4);
 		//console log to see scores currently placed on crystals
 	}
-	//show player winning
+	//shows player winning
 	function youWin() {
 		$("#winOrLose").text("YOU WIN!");
 		wins++;
 		$("#wins").text(wins);
 	}
-	//show player losing
+	//shows player losing
 	function youLose() {
 		$("#winOrLose").text("YOU LOSE!");
 		losses++;
@@ -51,19 +48,18 @@ $(document).ready(function() {
 	//fix error how prints equal "NaN"
 	//new game
 	function startGame() {
-		$(".crystalimg").hover(function() {
+		$(".crystalimg").hover(function () {
 			$(this).css({
 				opacity: .7
 			});
-		}, function() {
+		}, function () {
 			$(this).css({
 				opacity: 1
 			});
 		});
-		//copied from a word doc that I had written on
 		// The parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN.
 		// return: when a return statement is used in a function body, the execution of the function is stopped. 
-		$(".crystalimg").on("click", function() {
+		$(".crystalimg").on("click", function () {
 			if (totalScore >= magicNum) {
 				return;
 			}
@@ -73,14 +69,13 @@ $(document).ready(function() {
 			$("#totalScore").text(totalScore);
 			//console log for the crystalvalue to see if game is running
 			//if else statement for winning/losing
-			//copied from word doc
 			if (totalScore === magicNum) {
 				youWin();
 			} else if (totalScore > magicNum) {
 				youLose();
 			}
 		});
-		$(".btn").on("click", function() {
+		$(".btn").on("click", function () {
 			newGame();
 		});
 	}
